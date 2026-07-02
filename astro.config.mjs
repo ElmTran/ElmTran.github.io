4c75d0f4119ea1b1936f4eb17a1eb0caafdc8dd5
ElmTran <c897611977@gmail.com>
2026-07-02 17:45:54 +0800

    feat: add sitemap integration to Astro configuration
    - Integrated the @astrojs/sitemap package to enhance SEO capabilities by generating a sitemap for the site.
    - Updated the Astro configuration to include the new integration and adjusted the package.json and pnpm-lock.yaml files accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://elmtran.com",
   trailingSlash: "always",
+  integrations: [sitemap()],
   redirects: {
     "/album/": "/photos/",
     "/welcome/": "/",
